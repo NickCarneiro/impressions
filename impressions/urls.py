d0920b0b40574af5e5f25b68dedae0bfbcb850d6
Nick Carneiro <burthawk101@gmail.com>
2013-09-03 23:34:01 -0500

    got homepage template rendering

--- impressions/urls.py
+++ impressions/urls.py
@@ -1,17 +1,12 @@
 from django.conf.urls import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+import django.contrib.admin as admin
 
+admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'impressions.views.home', name='home'),
-    # url(r'^impressions/', include('impressions.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^$', 'impapp.views.home', name='home'),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
